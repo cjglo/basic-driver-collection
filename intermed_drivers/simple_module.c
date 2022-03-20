ff377e20f037cfd41e005fb252ac1ed9a752ed9c
cjglo <chris.j.gallo3@gmail.com>
2022-03-20 18:43:31 -0400

    learned and added __init

--- intermed_drivers/simple_module.c
+++ intermed_drivers/simple_module.c
@@ -2,23 +2,20 @@
 #include <linux/module.h>
 #include "simple_module.h"
 
-// printk would appear in real terminal, but terminals application
-// is not the same.  These will be in jounral (can see with `journalctl -f`)
-
-int simple_module_init(void) {
+// __init is kayword that marks that once initialized, function is no longer needed
+// aka it frees RAM since keeping this function isn't necessary
+__init int simple_module_init(void) {
 
 	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
 	return 0;
 }
+
 
 void simple_module_exit(void) {
 
 	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
 }
 
-// names are exactly what they sound like;
-// 1 that run at kernel compile or module load
-// 1 that runs when done 
 module_init(simple_module_init);
 module_exit(simple_module_exit);
 
